a4d79c9c61e121190f647cbfa885a8a46b4c61a3
Vegard Andreas Larsen <vegard@digitalcreations.no>
2014-06-26 17:27:38 +0200

    Show relevant debug information when response content is not a string

--- src/Router/Exceptions/ResponseContentIsNotStringException.php
+++ src/Router/Exceptions/ResponseContentIsNotStringException.php
@@ -2,7 +2,8 @@
 namespace DC\Router\Exceptions;
 
 class ResponseContentIsNotStringException extends \Exception {
-    function __construct() {
-        parent::__construct("The generated response content is not a string.");
+    function __construct(\DC\Router\IRequest $request) {
+        parent::__construct(
+            sprintf("The generated response content is not a string when requesting %s %s.", $request->getMethod(), $request->getPath()));
     }
 } 
