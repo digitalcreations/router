ffc8962759f5e75d7fafded64468fd61f58a2e2c
Vegard Andreas Larsen <vegard@digitalcreations.no>
2014-10-29 10:53:45 +0100

    Fix issue with dependency injection.

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -30,7 +30,7 @@ class Router {
      * @param IRouteFactory $routeFactory
      * @param IResponseWriter $responseWriter
      * @param IClassFactory $classFactory
-     * @param $filters array|IGlobalFilter[]
+     * @param $filters array|\DC\Router\IGlobalFilter[]
      */
     function __construct(IRouteMatcher $routeMatcher,
                          IRouteFactory $routeFactory,
